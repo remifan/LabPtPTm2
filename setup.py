08fec627ed9e0343eea09c2a21ee2769a0a45660
Remi Qirui FAN <fanqirui318@gmail.com>
2021-09-28 17:58:37 +0800

    update version number for a release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='labptptm2',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     install_requires=[
         'zarr[jupyter]==2.9.5',
